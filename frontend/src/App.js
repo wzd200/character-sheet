18f5594122ceca8a706415508c4c32df419ac21f
wzd200 <william.z.dye@gmail.com>
2023-04-02 20:39:28 -0400

    Folder cleanup and rebuild

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import MediaCard from './Components/Card';
+
 function App() {
   return (
     <div className="App">
       <h1>Character Sheet Application</h1>
+      <MediaCard></MediaCard>
     </div>
   );
 }
